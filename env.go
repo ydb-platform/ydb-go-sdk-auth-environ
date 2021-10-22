7c4736e0af19bcfa86d5a1755fdfdfb5a3497c23
asmyasnikov <79263256394@ya.ru>
2021-10-22 16:21:28 +0300

    update ydb-go-sdk

---
+++ env.go
@@ -0,0 +1,25 @@
+package environ
+
+import (
+	"context"
+	"os"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3"
+	"github.com/ydb-platform/ydb-go-yc"
+)
+
+func WithEnvironCredentials(ctx context.Context) ydb.Option {
+	if serviceAccountKeyFile, ok := os.LookupEnv("YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"); ok {
+		return yc.WithServiceAccountKeyFileCredentials(serviceAccountKeyFile)
+	}
+	if os.Getenv("YDB_ANONYMOUS_CREDENTIALS") == "1" {
+		return ydb.WithAnonymousCredentials()
+	}
+	if os.Getenv("YDB_METADATA_CREDENTIALS") == "1" {
+		return yc.WithMetadataCredentials(ctx)
+	}
+	if accessToken, ok := os.LookupEnv("YDB_ACCESS_TOKEN_CREDENTIALS"); ok {
+		return ydb.WithAccessTokenCredentials(accessToken)
+	}
+	return yc.WithMetadataCredentials(ctx)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ydb-platform/ydb-go-sdk-auth-environ
 go 1.16
 
 require (
-	github.com/ydb-platform/ydb-go-sdk-auth-iam v0.0.0-20210924070104-b5f3a952043b
-	github.com/ydb-platform/ydb-go-sdk/v3 v3.0.0-20210924064430-eba76bae10c9
+	github.com/ydb-platform/ydb-go-sdk/v3 v3.0.0
+	github.com/ydb-platform/ydb-go-yc v0.0.1
 )
