b83accd19fbc941bc0071f76ca2745bceaa759b8
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2024-05-16 18:26:25 +0300

    fix

--- version.go
+++ version.go
@@ -2,8 +2,8 @@ package environ
 
 const (
 	VersionMajor = "0"
-	VersionMinor = "2"
-	VersionPatch = "0"
+	VersionMinor = "3"
+	VersionPatch = "1"
 )
 
 const Version = VersionMajor + "." + VersionMinor + "." + VersionPatch
