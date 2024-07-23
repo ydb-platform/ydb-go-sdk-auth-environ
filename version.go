67983430d1485fd9cbfbe1ddf6243a4cd85cd596
Timofey Koolin <rekby@users.noreply.github.com>
2024-07-23 18:55:44 +0300

    Update version.go

--- version.go
+++ version.go
@@ -2,8 +2,8 @@ package environ
 
 const (
 	VersionMajor = "0"
-	VersionMinor = "4"
-	VersionPatch = "2"
+	VersionMinor = "5"
+	VersionPatch = "0"
 )
 
 const Version = VersionMajor + "." + VersionMinor + "." + VersionPatch
